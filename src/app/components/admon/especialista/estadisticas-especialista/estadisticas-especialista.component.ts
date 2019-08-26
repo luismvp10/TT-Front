1bdd011227b35957fc31f347a9d8adaa17956af0
Luis <luismvalenp@gmail.com>
2019-08-26 01:19:57 -0500

    Gráficas con chart.js

--- src/app/components/admon/especialista/estadisticas-especialista/estadisticas-especialista.component.ts
+++ src/app/components/admon/especialista/estadisticas-especialista/estadisticas-especialista.component.ts
@@ -4,6 +4,7 @@ import { SubshipmentService } from '../../../../services/subshipment/subshipment
 import { SectionService } from '../../../../services/section/section.service';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import  Swal  from 'sweetalert2';
+import {Chart} from 'chart.js';
 
 
 @Component({
@@ -12,6 +13,11 @@ import  Swal  from 'sweetalert2';
   styleUrls: ['./estadisticas-especialista.component.css']
 })
 export class EstadisticasEspecialistaComponent implements OnInit {
+  title = 'Graphics with Chart.js';
+  LineChart=[];
+  BarChart=[];
+  PieChart=[];
+
   SelectedChapter: string;
 //@ViewChild('chapter') childOne:SelectChapterComponent;
 //messsage ="Hola prro";
@@ -39,7 +45,118 @@ export class EstadisticasEspecialistaComponent implements OnInit {
   }
 
   ngOnInit() {
+this.LineChart = new Chart('lineChart', {
+  type: 'line',
+data: {
+ labels: [ "Jan", "Feb", "March", "April", "May", "June","July","Aug","Sep","Oct","Nov","Dec"],
+ datasets: [{
+     label: 'Number of Items Sold in Months',
+     data: [9, 7 , 3, 5, 2, 10,15,16,19,3,1,9],
+     fill: false,
+     lineTension: 0.2,
+     borderColor: "red",
+     borderWidth: 1
+ }]
+},
+options: {
+ title:{
+     text:"Line Chart",
+     display:true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
 
+
+// Bar chart:
+this.BarChart = new Chart('barChart', {
+  type: 'bar',
+data: {
+ labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+ datasets: [{
+     label: '# of Votes',
+     data: [9,7 , 3, 5, 2, 10],
+     backgroundColor: [
+         'rgba(255, 99, 132, 0.2)',
+         'rgba(54, 162, 235, 0.2)',
+         'rgba(255, 206, 86, 0.2)',
+         'rgba(75, 192, 192, 0.2)',
+         'rgba(153, 102, 255, 0.2)',
+         'rgba(255, 159, 64, 0.2)'
+     ],
+     borderColor: [
+         'rgba(255,99,132,1)',
+         'rgba(54, 162, 235, 1)',
+         'rgba(255, 206, 86, 1)',
+         'rgba(75, 192, 192, 1)',
+         'rgba(153, 102, 255, 1)',
+         'rgba(255, 159, 64, 1)'
+     ],
+     borderWidth: 1
+ }]
+},
+options: {
+ title:{
+     text:"Bar Chart",
+     display:true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
+
+// pie chart:
+this.PieChart = new Chart('pieChart', {
+  type: 'pie',
+data: {
+ labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+ datasets: [{
+     label: '# of Votes',
+     data: [9,7 , 3, 5, 2, 10],
+     backgroundColor: [
+         'rgba(255, 99, 132, 0.2)',
+         'rgba(54, 162, 235, 0.2)',
+         'rgba(255, 206, 86, 0.2)',
+         'rgba(75, 192, 192, 0.2)',
+         'rgba(153, 102, 255, 0.2)',
+         'rgba(255, 159, 64, 0.2)'
+     ],
+     borderColor: [
+         'rgba(255,99,132,1)',
+         'rgba(54, 162, 235, 1)',
+         'rgba(255, 206, 86, 1)',
+         'rgba(75, 192, 192, 1)',
+         'rgba(153, 102, 255, 1)',
+         'rgba(255, 159, 64, 1)'
+     ],
+     borderWidth: 1
+ }]
+},
+options: {
+ title:{
+     text:"Bar Chart",
+     display:true
+ },
+ scales: {
+     yAxes: [{
+         ticks: {
+             beginAtZero:true
+         }
+     }]
+ }
+}
+});
   }
   changeChapter(id_chapter) {
 
