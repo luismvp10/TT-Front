c6b83b2d748f2322749f727495ed5e8a01a7ff52
Luis <luismvalenp@gmail.com>
2019-09-22 20:25:33 -0500

    Pantalla gral con funcionalidad y responsividad

--- src/app/components/general/estadisticas/estadisticas.component.ts
+++ src/app/components/general/estadisticas/estadisticas.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ShipmentService} from "../../../services/shipment/shipment.service";
-import {SubshipmentService} from "../../../services/subshipment/subshipment.service";
-import {SectionService} from "../../../services/section/section.service";
+import {ShipmentService} from '../../../services/shipment/shipment.service';
+import {SubshipmentService} from '../../../services/subshipment/subshipment.service';
+import {SectionService} from '../../../services/section/section.service';
+import {TransactionService} from '../../../services/transaction/transaction.service';
 
 @Component({
   selector: 'app-estadisticas',
@@ -12,20 +13,34 @@ export class EstadisticasComponent implements OnInit {
   shipments: any = [];
   subshipments: any = [];
   sections: any = [];
+  transactions: any = [];
 
   chapterID: number;
   shipmentID: number;
   subShipmentID: number;
   sectionID: number;
-  countryID: string;
+  countryID: string = 'Todos';
   monthID: string;
   yearID: number;
+
+  monthArray = [
+    ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    [true, true, true, true, true, true, true, true, true, true, true, true]
+  ];
+  tempstatus = [false, false, false, false, false, false, false, false, false, false, false, false];
+  months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  totalExporta = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+  totalImporta = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+  params: any = [];
 
 
   constructor(private shipment: ShipmentService,
               private subshipment: SubshipmentService,
-              private section: SectionService
-              ) { }
+              private section: SectionService,
+              private transaction: TransactionService) {
+
+  }
 
   ngOnInit() {
   }
@@ -85,14 +100,66 @@ export class EstadisticasComponent implements OnInit {
     this.yearID = id_year;
   }
 
-
+  // Verifica que un pais tenga transacciones en los campos imports y exports
+  countryhastransaction(country, kind) {
+    if (country.imports.length < 1 && kind === 1) {
+      return false;
+    } else if (country.exports.length < 1 && kind === 2) {
+      return false;
+    }
+    return true;
+  }
   transacciones(){
+    this.tempstatus = [true, true, true, true, true, true, true, true, true, true, true, true];
     console.log("Información para la búsqueda");
     console.log("Sección " + this.sectionID);
-    console.log("Subpartida" +this.subShipmentID);
-    console.log("Partida" +this.shipmentID);
-    console.log("Capítulo" +this.chapterID);
+    console.log("Subpartida " +this.subShipmentID);
+    console.log("Partida " +this.shipmentID);
+    console.log("Capítulo " +this.chapterID);
     console.log("Año " + this.yearID);
+
+    this.totalExporta = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    this.totalImporta = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+    this.params.push({
+      section: this.sectionID,
+      subShipment: this.subShipmentID,
+      shipment: this.shipmentID,
+      chapter: this.chapterID,
+      month: this.months,
+      year: this.yearID,
+      country: this.countryID
+
+    });
+
+    this.transaction.getTransactions(this.params)
+      .subscribe( ( data: any) => {
+        this.transactions = data;
+
+        /*Suma de valores totales*/
+        this.transactions.forEach(element => {
+          element.exports.forEach(item => {
+            this.totalExporta[0][item.month - 1] += item.price;
+            this.totalExporta[1][item.month - 1] += item.weight;
+          });
+          element.imports.forEach(item => {
+            this.totalImporta[0][item.month - 1] += item.price;
+            this.totalImporta[1][item.month - 1] += item.weight;
+          });
+        });
+
+        // let i = 0;
+        // this.tempstatus.forEach(item => {
+        //   this.monthArray[1][i] = item;
+        //   i++;
+        // });
+
+
+
+      });
+
+    this.params = [];
+
   }
 
 }
